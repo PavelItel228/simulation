0d1f510078d930a736cd374fd92fbffb18162bd5
Ivaschenko Pavlo <44823566+PavelItel228@users.noreply.github.com>
2020-05-16 20:20:48 +0300

    Update Runner.java

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -11,21 +11,22 @@ import java.util.Scanner;
 public class Runner {
  // Считываем данные с консоли и открываем главное окно
 	public static void main(String[] args) {
-		Scanner myObj = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Which preferences do you want to use?");
 		System.out.println("1. Standart");
 		System.out.println("2. Custom");
 		System.out.println("Enter 1 or 2");
-		if (myObj.nextInt() == 2) {
+		if (scanner.nextInt() == 2) {
 			System.out.println("Enter average age of death");
-			Human.setLifeDuration(myObj.nextInt());
+			Human.setLifeDuration(scanner.nextInt());
 			System.out.println("Enter pregnancy chance");
-			Woman.setPregnancyChance(myObj.nextInt());
+			Woman.setPregnancyChance(scanner.nextInt());
 			System.out.println("Enter fruit drop chance");
-			FruitPlant.setFruitDropChance(myObj.nextInt());
+			FruitPlant.setFruitDropChance(scanner.nextInt());
 			System.out.println("Enter girl born chance");
-			Woman.setBornChance(myObj.nextInt());
+			Woman.setBornChance(scanner.nextInt());
 		}
+		scanner.close()
 		new XMainWindow();
 	}
 
